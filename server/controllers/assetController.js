b92e9bcbcd7cd7c989e217c804280aa6ccd753f0
David <github@djs.gg>
2022-03-02 01:03:35 -0500

    assetController

--- server/controllers/assetController.js
+++ server/controllers/assetController.js
@@ -1,12 +1,24 @@
-const path = require('path');
-
-const db = require(path.resolve(__dirname, '../models/assetModels.js'));
+const db = require  ('../models/assetModels.js')
 
 const assetController = {};
 
 
 assetController.getAssets = (req, res, next) => {
+    console.log('at getAssets controller');
+    const query = 'SELECT * FROM passwords;';
+    const assets = new Promise((resolve, reject) => {
+      const result = db.query(query);
+      return resolve(result);
+    })
+      .then((data) =>{
+          console.log(data);
+          '
+        res.locals.passwrords = data.rows;
+        return next();
+      })
+      .catch((error) => {console.log(error);});
+  };
 
-};
+
 
 module.exports = assetController;
