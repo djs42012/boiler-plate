9ccd3cf9bed0de380e01df01c503ef4ac3b91912
david <github@djs.gg>
2022-03-01 19:35:42 -0500

    clean webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require ('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
+    mode: process.env.NODE_ENV,
     entry: './client/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
@@ -35,8 +35,11 @@ module.exports = {
     },
     devServer: {
         static: {
-          publicPath: '/',
-          directory: path.resolve(__dirname)
-        }
-      },
+          directory: path.resolve(__dirname, 'build'),
+          publicPath: '/'
+        },
+        port: 8888,
+        compress: true,
+        hot: true,
+     },
 };
